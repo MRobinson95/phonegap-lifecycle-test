32a0f9b891d24730341c4e4e11ea32558ebcf88e
MRobinson95 <ROBM2_13@uni.worc.ac.uk>
2014-10-17 10:20:46 +0100

    asdasdasdasdasd

--- www/js/index.js
+++ www/js/index.js
@@ -8,8 +8,11 @@
     }
 	
 	function updateDisplay() {
-		$("#value").text("Value is: " + window.localStorage.getItem("Book"));
-		
+		$("#value").text("Value is: " + window.localStorage.getItem("Pen"));
+		$("#value1").text("Value is: " + window.localStorage.getItem("Book"));
+		$("#value2").text("Value is: " + window.localStorage.getItem("Food"));
+		$("#value3").text("Value is: " + window.localStorage.getItem("Drink"));
+		$("#value4").text("Value is: " + window.localStorage.getItem("Pen1"));
 	}
 
 
