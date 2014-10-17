946e005989b9375a4a0f2bd7f6c9bccc679459f9
MRobinson95 <ROBM2_13@uni.worc.ac.uk>
2014-10-17 10:19:46 +0100

    asdasd

--- www/js/index.js
+++ www/js/index.js
@@ -8,11 +8,8 @@
     }
 	
 	function updateDisplay() {
-		$("#value").text("Value is: " + window.localStorage.getItem("Pen"));
-		$("#value1").text("Value is: " + window.localStorage.getItem("Book"));
-		$("#value2").text("Value is: " + window.localStorage.getItem("Food"));
-		$("#value3").text("Value is: " + window.localStorage.getItem("Drink"));
-		$("#value4").text("Value is: " + window.localStorage.getItem("Pen1"));
+		$("#value").text("Value is: " + window.localStorage.getItem("Book"));
+		
 	}
 
 
