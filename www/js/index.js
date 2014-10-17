7913fcfa99b1d37cad85025ef3901a85e1c0ec00
MRobinson95 <ROBM2_13@uni.worc.ac.uk>
2014-10-17 10:41:55 +0100

    Carbs

--- www/js/index.js
+++ www/js/index.js
@@ -1,21 +1,43 @@
-    function storage() {
+    var chocoInfo = {"Name": "", "Energy": "", "Carbs": "", "Fat": "");
+	var chocoString = "";
+	var getChoco = "";
+	var chocoJSON = "";
+	
+	function storage() 
+	{
         storeData("Pen", "Red")
 		storeData("Book", "Boring")
 		storeData("Food", "Haribo")
 		storeData("Drink", "Gone")
 		storeData("Pen1", "Blue")
+		chocolate("Mars", "1888", "68.9", "17.1");
+		getChoco = window.localStorage.getItem("info");
+		chocoJSON = JSON.parse(getChoco);
 		updateDisplay();
+		
     }
 	
-	function updateDisplay() {
-		$("#value").text("Value is: " + window.localStorage.getItem("Pen"));
-		$("#value1").text("Value is: " + window.localStorage.getItem("Book"));
-		$("#value2").text("Value is: " + window.localStorage.getItem("Food"));
-		$("#value3").text("Value is: " + window.localStorage.getItem("Drink"));
-		$("#value4").text("Value is: " + window.localStorage.getItem("Pen1"));
+	function updateDisplay() 
+	{
+		$("#value").text("Value is: " + window.localStorage.getItem("Pen1"));
+		$("#value1").text("Carbs are: " + chocoJSON.Carbs);
+		
+		
+		
 	}
 
 
-    function storeData(storageKey, storageValue) {
+    function storeData(storageKey, storageValue) 
+	{
 		window.localStorage.setItem(storageKey, storageValue);
     }
+	
+	function chocolate(Name, Energy, Carbs, Fat)
+	{
+		chocoInfo.Name = Name;
+		chocoInfo.Energy = Energy;
+		chocoInfo.Carbs = Carbs;
+		chocoInfo.Fat = Fat;
+		chocoString = JSON.stringify(chocoInfo);
+		window.localStorage.setItem("info", chocoString);
+	}
