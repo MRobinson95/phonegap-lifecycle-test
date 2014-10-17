c365cd2aa0cd1bf6c0ecf4117a4cb60bfd484e56
MRobinson95 <ROBM2_13@uni.worc.ac.uk>
2014-10-17 10:15:35 +0100

    update1

--- www/js/index.js
+++ www/js/index.js
@@ -1,11 +1,18 @@
     function storage() {
         storeData("Pen", "Red")
+		storeData("Book", "Boring")
+		storeData("Food", "Haribo")
+		storeData("Drink", "Gone")
+		storeData("Pen1", "Blue")
 		updateDisplay();
     }
 	
 	function updateDisplay() {
 		$("#value").text("Value is: " + window.localStorage.getItem("Pen"));
-		
+		$("#value1").text("Value is: " + window.localStorage.getItem("Book"));
+		$("#value2").text("Value is: " + window.localStorage.getItem("Food"));
+		$("#value3").text("Value is: " + window.localStorage.getItem("Drink"));
+		$("#value4").text("Value is: " + window.localStorage.getItem("Pen1"));
 	}
 
 
